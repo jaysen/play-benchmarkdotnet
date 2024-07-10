f17a7290f3af87446a8361659c5d185fd0199b14
Jaysen Naidoo <Jaysen.Naidoo@moyo.co>
2024-07-10 16:56:17 +0200

    Add project files.

---
+++ BenchMarkPlay/DateParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchMarkPlay;
+
+public class DateParser
+{
+    public int GetYearFromDateTime(string dateTimeAsString)
+    {
+        DateTime dateTime = DateTime.Parse(dateTimeAsString);
+        return dateTime.Year;
+    }
+
+    public int GetYearFromSplit(string dateTimeAsString)
+    {
+        string[] parts = dateTimeAsString.Split('-');
+        return int.Parse(parts[0]);
+    }
+
+    public int GetYearFromSubstringParse(string dateTimeAsString)
+    {
+        var indexOfHyphen = dateTimeAsString.IndexOf('-');
+        string year = dateTimeAsString.Substring(0,indexOfHyphen);
+        return int.Parse(year);
+    }
+
+    public int GetYearFromSpanParse(ReadOnlySpan<char> dateTimeAsSpan)
+    {
+        var indexOfHyphen = dateTimeAsSpan.IndexOf('-');
+        return int.Parse(dateTimeAsSpan.Slice(0, indexOfHyphen));
+    }
+
+    public int GetYearFromSubstringManual(string dateTimeAsString)
+    {
+        var indexOfHyphen = dateTimeAsString.IndexOf('-');
+        string year = dateTimeAsString[..indexOfHyphen];
+        int result = 0;
+
+        for (int i = 0; i < year.Length; i++)
+        {
+            result = result * 10 + (year[i] - '0');
+        }
+        return result;
+    }
+
+    public int GetYearFromSpanManual(ReadOnlySpan<char> dateTimeAsSpan)
+    {
+        var indexOfHyphen = dateTimeAsSpan.IndexOf('-');
+        var year = dateTimeAsSpan.Slice(0, indexOfHyphen);
+        int result = 0;
+
+        for (int i = 0; i < year.Length; i++)
+        {
+            result = result * 10 + (year[i] - '0');
+        }
+        return result;
+    }
+}
---
+++ BenchMarkPlay/DateTimeBenchmarks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using CommandLine;
+
+namespace BenchMarkPlay;
+
+[MemoryDiagnoser]
+//[SimpleJob(RunStrategy.ColdStart)]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+
+public class DateTimeBenchmarks
+{
+    private const string DateTime = "1972-05-14T23:00:00";
+    private static readonly DateParser parser = new();
+
+    [Benchmark]
+    public void GetYearFromDateTime()
+    {
+        parser.GetYearFromDateTime(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromStringSplit()
+    {
+        parser.GetYearFromSplit(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromStringParse()
+    {
+        parser.GetYearFromSubstringParse(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromSpanParse()
+    {
+        parser.GetYearFromSpanParse(DateTime.AsSpan());
+    }
+
+    [Benchmark]
+    public void GetYearFromStringManual()
+    {
+        parser.GetYearFromSubstringManual(DateTime);
+    }
+
+    [Benchmark]
+    public void GetYearFromSpanManual()
+    {
+        parser.GetYearFromSubstringManual(DateTime);
+    }
+}
